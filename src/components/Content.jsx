31bba0114e9b57bd1b68998db24ba5ac0764a4c8
David <davemgj@gmail.com>
2021-04-11 01:21:57 -0700

    create error handling in case location services off, or no lat, long available

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -5,19 +5,19 @@ import AppDescription from "./AppDescription";
 import QueryTimes from "./QueryTimes";
 
 const Content = () => {
-  const [longitude, setLongitude] = useState(null);
   const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
 
   useEffect(() => {
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition((position) => {
-        setLongitude(position.coords.longitude);
         setLatitude(position.coords.latitude);
+        setLongitude(position.coords.longitude);
       });
       if (longitude && latitude) {
         console.log("Position Available");
-        console.log("Your longitude is :", longitude);
         console.log("Your latitude is :", latitude);
+        console.log("Your longitude is :", longitude);
       }
     } else {
       console.log("Position Not Available");
@@ -29,7 +29,7 @@ const Content = () => {
       <Header />
       <div className="main">
         <AppDescription />
-        <QueryTimes longitude={longitude} latitude={latitude} />
+        <QueryTimes latitude={latitude} longitude={longitude} />
       </div>
     </div>
   );
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -8,29 +8,33 @@ const useFetch = (lat, long) => {
   const [error, setError] = useState(null);
 
   const handleRequest = () => {
-    setClicked(true);
-    const url = `http://localhost:8080/api/passtimes?lat=${lat}&long=${long}`;
-    fetch(url)
-      .then((res) => {
-        if (!res.ok) {
-          throw Error("Sorry, we could not fetch any data...");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setData(data);
-        setIsLoading(false);
-        setLoaded(true);
-        setError(null);
-      })
-      .catch((err) => {
-        if (err.name === "AbortError") {
-          console.log("Fetch aborted");
-        } else {
-          setError(err.message);
+    if (lat && long) {
+      setClicked(true);
+      const url = `http://localhost:8080/api/passtimes?lat=${lat}&long=${long}`;
+      fetch(url)
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("Sorry, we could not fetch any data...");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setData(data);
           setIsLoading(false);
-        }
-      });
+          setLoaded(true);
+          setError(null);
+        })
+        .catch((err) => {
+          if (err.name === "AbortError") {
+            console.log("Fetch aborted");
+          } else {
+            setError(err.message);
+            setIsLoading(false);
+          }
+        });
+    } else {
+      setError("Please turn location services on");
+    }
   };
 
   return { data, clicked, isLoading, loaded, error, handleRequest };
