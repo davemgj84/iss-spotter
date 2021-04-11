f518b538050ea8ebdb34a03f4da0b406f466d961
David <davemgj@gmail.com>
2021-04-11 00:59:25 -0700

    update error message

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -4,7 +4,7 @@ const NotFound = (props) => {
   return (
     <div className="error">
       <h2>Sorry...</h2>
-      <p className="error-message">"{props.message}"...</p>
+      <p className="error-message">"Error: {props.message}"...</p>
       <p>We could not find the ISS right now...</p>
       <p>Please try again later...</p>
     </div>
