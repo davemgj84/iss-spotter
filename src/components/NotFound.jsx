473f50465b745003be103d1bbec8c511b28cc315
David <davemgj@gmail.com>
2021-04-15 15:04:14 -0700

    improved error messages - styling

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -5,7 +5,9 @@ const NotFound = (props) => {
   return (
     <div className="error">
       <h2>Sorry...</h2>
-      <p className="error-message">"{props.message}..."</p>
+      <p className="error-message">
+        <i className="fas fa-exclamation-triangle"></i>"{props.message}..."
+      </p>
       <p>We could not find the ISS right now...</p>
       <p>Please try again later...</p>
     </div>
